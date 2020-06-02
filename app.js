1340389f27b24c66691791d188c3e402ea2a1b7a
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2020-06-01 19:03:34 -0500

    Bug Fix Temperature Conversion

--- app.js
+++ app.js
@@ -1,108 +1,97 @@
-window.addEventListener('load', () => {
-    let long;
-    let lat;
-    let locationName = document.querySelector(".location-name");
-    let locationTimeZone = document.querySelector(".location-timezone");
-    let locationLocalTime = document.querySelector(".location-localtime");
-
-    let temperatureDescription = document.querySelector(".temperature-description");
-    let temperatureDegree = document.querySelector(".temperature-degree");
-    let feelsLikeTemp = document.querySelector(".feelslike");
-
-    let weatherIcon = document.querySelector(".icon");
-    let degreeSection = document.querySelector(".degree-section");
-    let degreeSpan = document.querySelector(".temperature-symb");
-    let locationBtn = document.querySelector(".getLocation");
-    let errorBox = document.querySelector(".errorBox");
-    let tempPill = document.querySelector(".tempPill");
-    let humidityLab = document.querySelector(".humidity");
-    let cloudcoverLab = document.querySelector(".cloudcover");
-    tempPill.textContent = "Fahrenite";
-
-    let searchInput=document.querySelector(".search-box");
-    errorBox.hidden = true;
-
-    searchInput.addEventListener('keypress', (evt) => 
-    {
-        if (evt.keyCode == 13)
-        {   
-            console.log('Enter Pressed' + 'Text: ' + searchInput.value)
-            const api = `${proxy}http://api.weatherstack.com/current?access_key=${key}&query=${searchInput.value}`;
-            callAPI(api)
-            searchInput.value = ''
-        }
-        
-    })
-
-    const getGeoLocation = () =>
-    {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                long = position.coords.longitude;
-                lat = position.coords.latitude;
-
-                const api = `${proxy}http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}`;
-
-                callAPI(api);
-            });
-
-        }
+window.addEventListener("load", () => {
+let searchInput = document.querySelector(".search-box");
+  let errorBox = document.querySelector(".errorBox");
+  errorBox.hidden = true;
+  let lat, long;
+  let locationName = document.querySelector(".location-name");
+  let locationTimeZone = document.querySelector(".location-timezone");
+  let locationLocalTime = document.querySelector(".location-localtime");
+  let temperatureSection = document.querySelector(".temperature");
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let celsiusTemp, fahreniteTemp;
+  let tempSymb = document.querySelector(".temperature-symb");
+  let feelsLikeTemp = document.querySelector(".feelslike");
+  let tempFlag = 1; // 1 = Celsius : 0 = Fahrenite
+  let weatherIcon = document.querySelector(".icon");
+  let locationBtn = document.querySelector(".getLocation");
+  let tempPill = document.querySelector(".tempPill");
+  tempPill.textContent = "Fahrenite";
+  let humidityLab = document.querySelector(".humidity");
+  let cloudcoverLab = document.querySelector(".cloudcover");
+  
+  searchInput.addEventListener("keypress", (evt) => {
+    if (evt.keyCode == 13) {
+      console.log("Enter Pressed" + "Text: " + searchInput.value);
+      const url = `${proxy}http://api.weatherstack.com/current?access_key=${key}&query=${searchInput.value}`;
+      callAPI(url);
+      searchInput.value = "";
     }
+  });
 
-    getGeoLocation()
-    
-    const key = '7834fd4e3be2f0300356603b771ee2db';
-    const proxy = ''
-    const proxy1='https://cors-anywhere.herokuapp.com/';
-
-    const callAPI = api => {
-        console.log("callAPI clicked.")
-        fetch(api)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                errorBox.hidden = true;
-                console.log(data);
-                const { temperature, weather_descriptions, weather_icons, humidity, cloudcover } = data.current;
-                const { name, region, localtime, timezone_id } = data.location;
-
-                temperatureDegree.textContent = temperature;
-                humidityLab.textContent = humidity + ' %';
-                cloudcoverLab.textContent = cloudcover + ' %';
-                temperatureDescription.textContent = weather_descriptions;
-                weatherIcon.innerHTML = `<img src=\"${weather_icons}\" width=\"64px\" height=\"64px\">`;
-
-
-                locationName.textContent = name + ', ' + region;
-                locationLocalTime.textContent = localtime;
-                locationTimeZone.textContent = timezone_id;
-
-                let Fahrenite = (temperature * (9 / 5)) + 32;
-                degreeSection.addEventListener('click', () => {
-                    if (degreeSpan.textContent === "C") {
-                        temperatureDegree.textContent = Math.floor(Fahrenite);
-                        degreeSpan.textContent = "F";
-                        tempPill.textContent = "Celsius";
-                    } else {
-                        temperatureDegree.textContent = temperature;
-                        degreeSpan.textContent = "C";
-                        tempPill.textContent = "Fahrenite";
-                    }
-                });
-
-            })
-            .catch(err => {
-                console.log(`Error| ${err}`);
-                errorBox.hidden = false;
-            });;
+  temperatureSection.addEventListener("click", () => {
+    if (tempFlag === 1) {
+      temperatureDegree.textContent = Math.floor(fahreniteTemp);
+      tempSymb.textContent = "F";
+      tempPill.textContent = "Celsius";
+      tempFlag = 0;
+    } else if (tempFlag === 0) {
+      temperatureDegree.textContent = celsiusTemp;
+      tempSymb.textContent = "C";
+      tempPill.textContent = "Fahrenite";
+      tempFlag = 1;
     }
+  });
 
-    locationBtn.addEventListener('click', () => {
-        getGeoLocation()
-    });
+  const getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
 
-    
+        const url = `${proxy}http://api.weatherstack.com/current?access_key=${key}&query=${lat},${long}`;
+        callAPI(url);
+      });
+    }
+  };
+
+  getGeoLocation();
+
+  const key = "7834fd4e3be2f0300356603b771ee2db";
+  const proxy = "";
+  const proxy1 = "https://cors-anywhere.herokuapp.com/";
+
+  const callAPI = (api) => {
+    console.log("callAPI clicked.");
+    fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        errorBox.hidden = true;
+        const { temperature, weather_descriptions, weather_icons, humidity, cloudcover, feelslike} = data.current;
+        const { name, region, localtime, timezone_id } = data.location;
+        celsiusTemp = temperature;
+
+        temperatureDegree.textContent = celsiusTemp;
+        humidityLab.textContent = humidity + " %";
+        cloudcoverLab.textContent = cloudcover + " %";
+        temperatureDescription.textContent = weather_descriptions;
+        weatherIcon.innerHTML = `<img src=\"${weather_icons}\" width=\"64px\" height=\"64px\">`;
+
+        locationName.textContent = name + ", " + region;
+        locationLocalTime.textContent = localtime;
+        locationTimeZone.textContent = timezone_id;
+
+        fahreniteTemp = celsiusTemp * (9 / 5) + 32;
+      })
+      .catch((err) => {
+        console.log(`Error| ${err}`);
+        errorBox.hidden = false;
+      });
+  };
+
+  locationBtn.addEventListener("click", () => {
+    getGeoLocation();
+  });
 });
-
-
