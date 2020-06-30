70b91d3d59b8c7e0efef7054f5034cfc8d086a5e
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2020-06-30 01:20:03 -0500

    Add proxy(CORS) to the API call

--- app.js
+++ app.js
@@ -63,8 +63,7 @@ let searchInput = document.querySelector(".search-box");
   getGeoLocation();
 
   const key = "7834fd4e3be2f0300356603b771ee2db";
-  const proxy = "";
-  const proxy1 = "https://cors-anywhere.herokuapp.com/";
+  const proxy= "https://cors-anywhere.herokuapp.com/";
 
   const callAPI = (api) => {
     console.log("callAPI clicked.");
